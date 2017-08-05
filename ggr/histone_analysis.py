9201a996e2dcc82dd93408f972cc3ad24038c652
vervacity <dskim89@gmail.com>
2017-08-04 17:19:42 -0700

    clean up counting reads code (works for ATAC + histone)

--- ggr/histone_analysis.py
+++ ggr/histone_analysis.py
@@ -10,164 +10,10 @@ import signal
 import pandas as pd
 
 from ggr.util.utils import add_folder
+from ggr.util.counting import make_count_matrix
 from ggr.util.bed_utils import merge_regions
 from ggr.util.parallelize import setup_multiprocessing_queue
 from ggr.util.parallelize import run_in_parallel
-
-
-
-def make_midpoints_file(bedpe_file, out_file):
-    """Get the midpoint base pair from a paired ended dataset.
-    This is the cleanest signal for where a factor was bound 
-    to the fragment.
-    """
-    assert ".bedpe.gz" in bedpe_file
-
-    # for multiprocessing purposes
-    signal.signal(signal.SIGPIPE, signal.SIG_DFL)
-    
-    # awk command
-    # for BEDPE, if first strand is positive, grab start of 1 and end of 2
-    # if first strand is negative, grab end of 1 and start of 2
-    make_midpoints = (
-        "zcat {0} | "
-        "awk -F '\t' 'BEGIN {{ OFS=\"\t\" }} "
-        "{{ if ($9==\"+\") {{ $2=$2+int(($6-$2)/2); $3=$2+int(($6-$2)/2)+1 }} "
-        "else if ($9==\"-\") {{ $2=$5+int(($3-$5)/2); $3=$5+int(($3-$5)/2)+1 }} "
-        "print $1\"\t\"$2\"\t\"$3\"\tN\t1000\t\"$9 }}' | "
-        "sort -k1,1 -k2,2n | "
-        "gzip -c "
-        "> {1}").format(
-            bedpe_file,
-            out_file)
-
-    print make_midpoints
-    os.system(make_midpoints)
-    
-    return None
-
-def get_counts_from_tagalign(
-        master_regions_file,
-        tagalign_file,
-        out_counts_file):
-    """Get counts in regions from tagAlign of reads
-    """
-    get_coverage = (
-        "bedtools coverage "
-        "-sorted "
-        "-counts "
-        "-a {0} -b {1} | "
-        "gzip -c > {2}").format(
-            master_regions_file,
-            tagalign_file,
-            out_counts_file)
-    print get_coverage
-    os.system(get_coverage)
-    
-    return
-
-
-def get_counts_in_regions_from_dir(
-        reads_dir,
-        master_regions_file,
-        out_dir,
-        unique_string="",
-        count_type="cuts"):
-    """given a directory with tagAlign files, get overlap
-    with master regions and save out to output folder
-    Use unique string if multiple assays in same folder and 
-    you only want one of them
-    """
-    count_queue = setup_multiprocessing_queue()
-
-    reads_files = sorted(
-        glob.glob("{0}/*{1}*{2}.tagAlign.gz".format(
-            reads_dir, unique_string, count_type)))
-    
-    for reads_file in reads_files:
-        count_file = "{0}/{1}.counts.txt.gz".format(
-            out_dir,
-            os.path.basename(reads_file).split(
-                ".{}".format(count_type))[0])
-        if not os.path.isfile(count_file):
-            print count_file
-            count_queue.put([
-                get_counts_from_tagalign,
-                [master_regions_file, reads_file, count_file]])
-
-    run_in_parallel(count_queue)
-
-    return
-
-
-def make_counts_mat_from_dir(counts_dir, out_mat_file):
-    """given a directory with counts files (from bedtools coverage)
-    merge to make a matrix file
-    """
-    counts_files = sorted(glob.glob('{}/*counts.txt.gz'.format(counts_dir)))
-    
-    coverage_df = pd.DataFrame()
-    if not os.path.isfile(out_mat_file):
-        for counts_file in counts_files:
-            filename_fields = os.path.basename(counts_file).split('.')
-
-            # some hackyness around names
-            header = '{0}_{1}'.format(
-                filename_fields[0].split('-')[1],
-                filename_fields[4])
-
-            # and now the counts
-            counts = pd.read_csv(counts_file, sep='\t', header=None)
-            counts.columns = ['chrom', 'start', 'stop', 'counts']
-
-            coverage_df[header] = counts['counts']
-        coverage_df = coverage_df.set_index(
-            counts['chrom'] + ':' + counts['start'].map(str) + '-' + counts['stop'].map(str))
-        coverage_df.to_csv(out_mat_file, sep='\t', index_label=False, compression='gzip')
-    
-    return
-
-
-def make_chipseq_count_matrix(
-        master_regions_file,
-        bedpe_file_list,
-        out_mat_file,
-        mark_name,
-        tmp_dir="."):
-    """For regions in master file list, return a matrix file 
-    with the counts of midpoints of fragments in these regions
-    """
-    # generate (sorted) midpoint files from bedpe files
-    midpoints_dir = "{}/midpoints".format(tmp_dir)
-    os.system("mkdir -p {}".format(midpoints_dir))
-    
-    midpoints_queue = setup_multiprocessing_queue()
-    for bedpe_file in bedpe_file_list:
-        midpoints_file = "{0}/{1}.midpoints.tagAlign.gz".format(
-            midpoints_dir,
-            os.path.basename(bedpe_file).split(".bedpe")[0])
-        if not os.path.isfile(midpoints_file):
-            print midpoints_file
-            midpoints_queue.put([
-                make_midpoints_file,
-                [bedpe_file, midpoints_file]])
-            
-    run_in_parallel(midpoints_queue)
-
-    # overlap with master regions and then run bedtools coverage
-    counts_dir = "{}/counts".format(tmp_dir)
-    os.system("mkdir -p {}".format(counts_dir))
-    get_counts_in_regions_from_dir(
-        midpoints_dir,
-        master_regions_file,
-        counts_dir,
-        unique_string=mark_name,
-        count_type="midpoints")
-
-    # merge all into matrix
-    make_counts_mat_from_dir(counts_dir, out_mat_file)
-    
-    return
 
 
 
@@ -232,7 +78,7 @@ def run(args):
         # make count matrix for histone mark
         args.chipseq["histones"][histone]["counts"] = "{0}/ggr.{1}.midpoints.counts.mat.txt.gz".format(
             args.folders["data_dir"], histone)
-        make_chipseq_count_matrix(
+        make_count_matrix(
             args.chipseq["histones"][histone]["master_regions"],
             bedpe_file_list,
             args.chipseq["histones"][histone]["counts"],
@@ -252,6 +98,11 @@ def run(args):
             print run_deseq2
             os.system(run_deseq2)
 
+            
+        # run rlog to have standardized matrix (to viz and do QC)
+        
+        
+        
             
         # enumerate trajectories (9 groups) and keep trajectory numbers
 
---
+++ ggr/util/counting.py
@@ -0,0 +1,286 @@
+"""Tools to quickly count reads in a region and analyze
+"""
+
+import os
+import glob
+import signal
+
+import pandas as pd
+
+from ggr.util.bed_utils import merge_regions
+from ggr.util.parallelize import setup_multiprocessing_queue
+from ggr.util.parallelize import run_in_parallel
+
+
+def make_ends_file(tagalign_file, out_file):
+    """Take a tagAlign file and get read ends only
+    Generally used for ATAC-seq/DNase-seq, where the key
+    information in the read is the END of the read (where 
+    the cut in the open site occurred)
+
+    Args:
+      tagalign_file: BED file of reads
+      out_file: BED file of "reads" (ie, 1bp read ends)
+    """
+    assert os.path.splitext(out_file)[1] == ".gz"
+    assert os.path.splitext(tagalign_file)[1] == ".gz"
+    
+    # Hack for python multiprocessing
+    signal.signal(signal.SIGPIPE, signal.SIG_DFL)
+    
+    # awk is faster than python
+    make_ends = (
+        "zcat {0} | "
+        "awk -F '\t' 'BEGIN {{ OFS=\"\t\" }} "
+        "{{ if ($6==\"+\") {{$3=$2+1}} "
+        "else if ($6==\"-\") {{$2=$3-1}} "
+        "print $0 }}' | "
+        "sort -k1,1 -k2,2n | "
+        "gzip -c "
+        "> {1}").format(
+            tagalign_file, out_file)
+    
+    print make_ends
+    os.system(make_ends)
+
+    return None
+
+
+def make_midpoints_file(bedpe_file, out_file):
+    """Get the midpoint base pair from a paired ended dataset.
+    This is the cleanest signal for where a factor was bound 
+    to the fragment, ie use for paired ended ChIP-seq data.
+
+    Args:
+      bedpe_file: BEDPE format file with 2 paired reads
+      out_file: BED file of "reads" (ie, 1bp midpoints)
+    """
+    assert ".bedpe.gz" in bedpe_file
+    assert os.path.splitext(bedpe_file)[1] == ".gz"
+    assert os.path.splitext(out_file)[1] == ".gz"
+
+    # for multiprocessing purposes
+    signal.signal(signal.SIGPIPE, signal.SIG_DFL)
+    
+    # awk is faster than python
+    make_midpoints = (
+        "zcat {0} | "
+        "awk -F '\t' 'BEGIN {{ OFS=\"\t\" }} "
+        "{{ if ($9==\"+\") {{ $2=$2+int(($6-$2)/2); $3=$2+int(($6-$2)/2)+1 }} "
+        "else if ($9==\"-\") {{ $2=$5+int(($3-$5)/2); $3=$5+int(($3-$5)/2)+1 }} "
+        "print $1\"\t\"$2\"\t\"$3\"\tN\t1000\t\"$9 }}' | "
+        "sort -k1,1 -k2,2n | "
+        "gzip -c "
+        "> {1}").format(
+            bedpe_file,
+            out_file)
+
+    print make_midpoints
+    os.system(make_midpoints)
+    
+    return None
+
+
+def adjust_reads_from_dir(reads_files, out_dir, adjustment="ends"):
+    """Use to do reads adjustments (get ends or midpoints)
+    by giving an input and output directory
+
+    Args:
+      reads_files: list of BED of reads filenames
+      out_dir: where to save them
+    """
+    # set up queue
+    adjust_queue = setup_multiprocessing_queue()
+
+    # go through reads files
+    for reads_file in reads_files:
+
+        # check adjustment type and set up accordingly
+        if adjustment == "ends":
+            out_file = "{0}/{1}.{2}.tagAlign.gz".format(
+                out_dir,
+                os.path.basename(reads_file).split('.tagAlign')[0],
+                adjustment)
+            adjust_fn = make_ends_file
+            
+        elif adjustment == "midpoints":
+            out_file = "{0}/{1}.{2}.tagAlign.gz".format(
+                out_dir,
+                os.path.basename(reads_file).split('.bedpe')[0],
+                adjustment)
+            adjust_fn = make_midpoints_file
+
+        else:
+            "Need proper adjustment type"
+            return
+
+        # add to queue
+        if not os.path.isfile(out_file):
+            print out_file
+            adjust_queue.put([
+                adjust_fn,
+                [reads_file, out_file]])
+
+    # run queue
+    run_in_parallel(adjust_queue)
+    
+    return None
+
+
+def get_counts_from_tagalign(
+        master_regions_file,
+        tagalign_file,
+        out_counts_file):
+    """Get counts in regions from tagAlign of reads
+
+    Args:
+      master_regions_file: master BED file
+      tagalign_file: BED file of reads
+      out_counts_file: output file with counts/region
+    """
+    assert os.path.splitext(out_counts_file)[1] == ".gz"
+
+    # run bedtools coverage
+    get_coverage = (
+        "bedtools coverage "
+        "-sorted "
+        "-counts "
+        "-a {0} -b {1} | "
+        "gzip -c > {2}").format(
+            master_regions_file,
+            tagalign_file,
+            out_counts_file)
+    
+    print get_coverage
+    os.system(get_coverage)
+    
+    return
+
+
+def get_counts_in_regions_from_dir(
+        reads_dir,
+        master_regions_file,
+        out_dir,
+        unique_string="",
+        count_type="cuts"):
+    """given a directory with tagAlign files, get overlap
+    with master regions and save out to output folder
+    Use unique string if multiple assays in same folder and 
+    you only want one of them
+
+    Args:
+      reads_dir: directory with BED files of reads
+      master_regions_file: BED file to get counts in
+      out_dir: where to put counts files
+      unique_string: for glob
+      count_type: for naming files
+    """
+    # setup parallelize queue
+    count_queue = setup_multiprocessing_queue()
+
+    # get reads files
+    reads_files = sorted(
+        glob.glob("{0}/*{1}*{2}.tagAlign.gz".format(
+            reads_dir, unique_string, count_type)))
+
+    # run reads_files
+    for reads_file in reads_files:
+        count_file = "{0}/{1}.counts.txt.gz".format(
+            out_dir,
+            os.path.basename(reads_file).split(
+                ".{}".format(count_type))[0])
+        if not os.path.isfile(count_file):
+            print count_file
+            count_queue.put([
+                get_counts_from_tagalign,
+                [master_regions_file, reads_file, count_file]])
+
+    run_in_parallel(count_queue)
+
+    return None
+
+
+def make_counts_mat_from_dir(counts_dir, out_mat_file):
+    """given a directory with counts files (from bedtools coverage)
+    merge to make a matrix file
+    
+    Args:
+      counts_dir: directory of counts files
+      out_mat_file: output matrix file of counts
+    """
+    # get counts files
+    counts_files = sorted(
+        glob.glob('{}/*counts.txt.gz'.format(
+            counts_dir)))
+
+    # join all the counts
+    coverage_df = pd.DataFrame()
+    if not os.path.isfile(out_mat_file):
+        for counts_file in counts_files:
+            filename_fields = os.path.basename(counts_file).split('.')
+
+            # some hackyness around names
+            header = '{0}_{1}'.format(
+                filename_fields[0].split('-')[1],
+                filename_fields[4])
+
+            # and now the counts
+            counts = pd.read_csv(counts_file, sep='\t', header=None)
+            counts.columns = ['chrom', 'start', 'stop', 'counts']
+            coverage_df[header] = counts['counts']
+            
+        coverage_df = coverage_df.set_index(
+            counts['chrom'] + ':' + counts['start'].map(str) + '-' + counts['stop'].map(str))
+
+        # write out
+        coverage_df.to_csv(out_mat_file, sep='\t', index_label=False, compression='gzip')
+    
+    return
+
+
+def make_count_matrix(
+        master_regions_file,
+        bedpe_file_list,
+        out_mat_file,
+        assay_name,
+        adjustment="midpoints",
+        tmp_dir="."):
+    """Given ChIP-seq (paired ended) or ATAC, get count matrix
+    Note that if ChIP-seq, it MUST be paired ended, with BEDPE
+    files. If ATAC, must have tagAlign files.
+
+    Args:
+      master_regions_file: regions you want to count in
+      bedpe_file_list: list of BEDPE formatted reads
+      out_mat_file: output file
+      mark_name: ChIP-seq mark name in file
+      tmp_dir: scratch dir as needed
+    """
+
+    # first adjust the reads files to get fragment midpoints
+    adjust_dir = "{0}/{1}".format(tmp_dir, adjustment)
+    os.system("mkdir -p {}".format(adjust_dir))
+    adjust_reads_from_dir(
+        bedpe_file_list,
+        adjust_dir,
+        adjustment=adjustment)
+
+    # then overlap the fragment midpoints with the master regions
+    counts_dir = "{}/counts".format(tmp_dir)
+    os.system("mkdir -p {}".format(counts_dir))
+    get_counts_in_regions_from_dir(
+        adjust_dir,
+        master_regions_file,
+        counts_dir,
+        unique_string=assay_name,
+        count_type=adjustment)
+
+    # finally merge all into matrix
+    make_counts_mat_from_dir(counts_dir, out_mat_file)
+
+    return None
+
+
+
+
+
