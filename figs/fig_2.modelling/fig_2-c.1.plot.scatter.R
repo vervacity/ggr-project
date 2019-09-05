52641cb5b9c7f13962cdc12d39f5664c5d7c5b4c
vervacity <dskim89@gmail.com>
2019-09-05 15:29:20 -0700

    fix fig vectors

--- figs/fig_2.modelling/fig_2-c.1.plot.scatter.R
+++ figs/fig_2.modelling/fig_2-c.1.plot.scatter.R
@@ -30,22 +30,26 @@ plot_scatter <- function(data, plot_file, colour, day) {
     # get R vals (spearman and pearson)
     r_s <- cor(data$x, data$y, method="spearman")
     r_s <- format(round(r_s, 3), nsmall=3)
-    r_s <- paste("R[S]==", r_s, sep="")
+    #r_s <- paste("R[S]==", r_s, sep="")
+    r_s <- paste("Rs=", r_s, sep="")
     
     r_p <- cor(data$x, data$y, method="pearson")
     r_p <- format(round(r_p, 3), nsmall=3)
-    r_p <- paste("R[P]==", r_p, sep="")
+    #r_p <- paste("R[P]==", r_p, sep="")
+    r_p <- paste("Rp=", r_p, sep="")
     
     # plot
     ggplot(data, aes(x=x, y=y)) +
-        geom_point(
-            alpha=0.7, shape=16, stroke=0, size=0.3, colour=colour) + # alpha= 0.7
+        #geom_point(
+        #    alpha=0.7, shape=16, stroke=0, size=0.3, colour=colour) + # alpha= 0.1 
         annotate(
-            "text", x=5, y=0.5, size=1.5, vjust="inward", hjust="inward",
-            label=r_s, parse=TRUE) +
-        annotate(
-            "text", x=5, y=0.1, size=1.5, vjust="inward", hjust="inward",
-            label=r_p, parse=TRUE) +
+            "text", x=5, y=0.5,
+            label=r_s, size=1.5,
+            vjust="inward", hjust="inward") +
+        #geom_text(
+        #    aes(label=r_s, x=5, y=0.5), size=1.5, vjust="inward", hjust="inward") +
+        #geom_text(
+        #    aes(label=r_p, x=5, y=0.1), size=1.5, vjust="inward", hjust="inward") +
         geom_abline(size=0.115, intercept=0, slope=1, color="gray", linetype="dashed") +
         labs(title=day, x="Predicted", y="Actual") +
         theme_bw() +
@@ -54,7 +58,8 @@ plot_scatter <- function(data, plot_file, colour, day) {
             text=element_text(family="ArialMT"),
             plot.title=element_text(size=8, margin=margin(b=0)),
             plot.margin=margin(5,1,1,1),
-            panel.background=element_blank(),
+            plot.background=element_rect(fill="transparent"),
+            panel.background=element_rect(fill="transparent"),
             panel.border=element_blank(),
             panel.grid=element_blank(),
             axis.title=element_text(size=6),
@@ -67,7 +72,41 @@ plot_scatter <- function(data, plot_file, colour, day) {
             axis.ticks.length=unit(0.01, "in")) +
        scale_x_continuous(limits=c(0,5), expand=c(0,0)) +
        scale_y_continuous(limits=c(0,5), expand=c(0,0))
-    ggsave(plot_file, height=1.25, width=1.25, units="in", useDingbats=FALSE)
+
+    ggsave("test.pdf", height=1.25, width=1.25, units="in", useDingbats=FALSE)
+    
+    ggplot(data, aes(x=x, y=y)) +
+        geom_point(
+            alpha=0.7, shape=16, stroke=0, size=0.3, colour=colour) + # alpha= 0.1
+        #geom_text(
+        #    aes(label=r_s, x=5, y=0.5), parse=TRUE, size=1.5, vjust="inward", hjust="inward") +
+        #geom_text(
+        #    aes(label=r_p, x=5, y=0.1), parse=TRUE, size=1.5, vjust="inward", hjust="inward") +
+        #geom_abline(size=0.115, intercept=0, slope=1, color="gray", linetype="dashed") +
+        labs(title=day, x="Predicted", y="Actual") +
+        theme_bw() +
+        theme(
+            aspect.ratio=1,
+            text=element_text(family="ArialMT", colour="transparent"),
+            plot.title=element_text(size=8, margin=margin(b=0)),
+            plot.margin=margin(5,1,1,1),
+            panel.background=element_blank(),
+            panel.border=element_blank(),
+            panel.grid=element_blank(),
+            axis.title=element_text(size=6),
+            axis.title.x=element_text(vjust=1, margin=margin(0,0,0,0)),
+            axis.title.y=element_text(vjust=1, margin=margin(0,0,0,0)),
+            axis.text.y=element_text(size=6, colour="transparent"),
+            axis.text.x=element_text(size=6, colour="transparent"),
+            axis.line=element_line(color="transparent", size=0.115, lineend="square"),
+            axis.ticks=element_line(size=0.115, color="transparent"),
+            axis.ticks.length=unit(0.01, "in")) +
+       scale_x_continuous(limits=c(0,5), expand=c(0,0)) +
+       scale_y_continuous(limits=c(0,5), expand=c(0,0))
+    ggsave("test.png", height=1.25, width=1.25, units="in")
+
+    quit()
+    #ggsave(plot_file, height=1.25, width=1.25, units="in", useDingbats=FALSE)
 
 }
 
