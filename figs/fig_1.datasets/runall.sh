414ef9b7fa766a31f855bc244126b6cc35081792
vervacity <dskim89@gmail.com>
2019-09-28 16:17:26 -0700

    updates to fig 1

---
+++ figs/fig_1.datasets/runall.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# code
+GIT_DIR=~/git/ggr-project
+SCRIPT_DIR=$GIT_DIR/figs/fig_1.skin_data
+
+# dirs
+GGR_DIR=/mnt/lab_data/kundaje/users/dskim89/ggr/integrative/v1.0.0a
+
+# dataset matrix
+#$SCRIPT_DIR/fig_1-b.0.plot.R $GIT_DIR/ggr/data/ggr_datasets.txt
+
+# PCA plots
+$SCRIPT_DIR/fig_1-c.0.plot.R fig_1-c.atac.pdf $GGR_DIR/results/atac/timeseries/plots/ggr.atac.ends.counts.rep1.rlog.dynamic.filt.mat.txt.gz $GGR_DIR/results/atac/timeseries/plots/ggr.atac.ends.counts.rep2.rlog.dynamic.filt.mat.txt.gz
+$SCRIPT_DIR/fig_1-c.0.plot.R fig_1-c.rna.pdf $GGR_DIR/results/rna/timeseries/matrices/ggr.rna.counts.pc.expressed.timeseries_adj.rep1.rlog.dynamic.mat.txt.gz $GGR_DIR/results/rna/timeseries/matrices/ggr.rna.counts.pc.expressed.timeseries_adj.rep2.rlog.dynamic.mat.txt.gz
+
+# gene set enrichment results
+#$SCRIPT_DIR_data/fig_1-c.1.plot.R $GGR_DIR/results/rna/timeseries/gsea/ggr.rna.gsea_results.txt.gz
+
+# vignettes - pulled from vis files
+
+
+# heatmaps
+
+
+
+# linking plots
