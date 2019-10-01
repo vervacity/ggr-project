498e8125231a09fb76042e26cd7ea94faf283287
vervacity <dskim89@gmail.com>
2019-10-01 14:49:53 -0700

    update to fig 3 vignette

--- figs/fig_3.motifs_and_tfs/runall.sh
+++ figs/fig_3.motifs_and_tfs/runall.sh
@@ -3,14 +3,22 @@
 SCRIPT_DIR=~/git/ggr-project/figs/fig_3.motifs_and_tfs
 SIG_MOTIFS_DIR=/mnt/lab_data/kundaje/users/dskim89/ggr/nn/inference.2019-03-12/motifs.sig/motifs.adjust.diff.rna_filt.dmim
 
-# fig 3 b
+GGR_DIR=/mnt/lab_data/kundaje/users/dskim89/ggr/integrative/v1.0.0a
+
+
+# vignette - visualize region
+# help choosing
+#$SCRIPT_DIR/choose_vignette.py
+#~/git/ggr-project/figs/fig_1.datasets/plot.rna_bar.R $GGR_DIR/results/rna/timeseries/matrices/ggr.rna.counts.pc.expressed.timeseries_adj.pooled.rlog.mat.txt.gz ENSG00000189182 gene_vals.KRT77.pdf
+
+# allelic imbalance ATAC to validate importance scores
 #$SCRIPT_DIR/fig_3-b.3.prep.py
 #$SCRIPT_DIR/fig_3-b.4.plot.R results.w_imptscore.txt results.sig_only.txt
 
-# fig 3 c
+# pwm x rna (supplement)
 PVALS_FILE=$SIG_MOTIFS_DIR/pvals.rna_filt.corr_filt.h5
-$SCRIPT_DIR/fig_3-c.0.plot.pwm_x_rna.R $PVALS_FILE pvals
+#$SCRIPT_DIR/fig_3-c.0.plot.pwm_x_rna.R $PVALS_FILE pvals
 
-# fig 3d, 3f
+# motifs and tfs
 SUMMARY_DIR=$SIG_MOTIFS_DIR/summary
 #$SCRIPT_DIR/fig_3-d.0.plot.motifs_and_tfs.R $SUMMARY_DIR/ggr.pwms_present_summary.txt $SUMMARY_DIR/ggr.pwms_patterns_summary.txt $SUMMARY_DIR/ggr.tfs_corr_summary.txt $SUMMARY_DIR/ggr.tfs_patterns_summary.txt
