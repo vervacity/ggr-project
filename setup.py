fefb62a8cd17f23bb687d2a947a1011f0083b39e
vervacity <dskim89@gmail.com>
2019-12-16 13:45:46 -0800

    update version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 
 if __name__== "__main__":
     setup(name="ggr-project",
-          version="0.1.0",
+          version="1.1.0",
           description="GGR analysis",
           author="Daniel Kim",
           author_email='danielskim@stanford.edu',
@@ -12,7 +12,6 @@ if __name__== "__main__":
           license="MIT",
           install_requires=[],
           packages=find_packages(),
-          #package_data={'ggr':'data/*.json'},
           package_data={"":["data/*.json", "data/*.txt"]},
           scripts=["bin/ggr"] + glob("R/*/*.R") + glob("R/*.R")
     )
