d18c5b3bdef5d3921434b5a9b88aaba774d09929
vervacity <dskim89@gmail.com>
2019-12-17 14:59:54 -0800

    install script for R packages used

---
+++ R/install.R
@@ -0,0 +1,18 @@
+
+# script of all relevant things to install
+
+
+# CRAN
+#install.packages("gplots", repos="http://cran.us.r-project.org")
+#install.packages("ggplot2", repos="http://cran.us.r-project.org")
+install.packages("ggsci", repos="http://cran.us.r-project.org")
+
+#install.packages("gridGraphics", repos="http://cran.us.r-project.org")
+#install.packages("gridExtra", repos="http://cran.us.r-project.org")
+
+#install.packages("gProfileR", repos="http://cran.us.r-project.org")
+
+
+
+
+
