fba54222ec92a6011a4f96f62928a7b28f07aa5e
vervacity <dskim89@gmail.com>
2021-02-18 20:39:00 -0800

    plot update

--- R/plot.compare_sig.venn.R
+++ R/plot.compare_sig.venn.R
@@ -7,11 +7,12 @@ args <- commandArgs(trailingOnly=TRUE)
 data_file <- args[1]
 plot_file <- args[2]
 
-
+# read in data
 data <- read.table(data_file, sep="\t", header=TRUE, row.names=1)
 data <- data * (as.numeric(rownames(data))+1)
 data <- as.list(data)
 
+# remove nulls
 for (l in names(data)) {
     elems <- data[[l]]
     elems <- elems[elems != 0]
@@ -21,15 +22,20 @@ for (l in names(data)) {
 # plot
 temp <- venn.diagram(
     data,
-    fill = c("red", "green"),
-    alpha = c(0.5, 0.5),
-    cex = 2,
-    #cat.fontface = 4,
-    lty =2,
-    #fontfamily =3,
-    filename = NULL)
+    fill=c("brown1", "cornflowerblue"),
+    main="Enriched motif identification:\nNeural Net Motifs vs HOMER+ATAC",
+    main.fontfamily="ArialMT",
+    main.cex=0.5,
+    alpha=c(0.5, 0.5),
+    cex=1,
+    lty=1,
+    lwd=1,
+    fontfamily="ArialMT",
+    cat.fontfamily="ArialMT",
+    cat.cex=0.5,
+    filename=NULL)
 
 # save out
-pdf(file=plot_file)
+pdf(file=plot_file, height=2, width=2)
     grid.draw(temp)
 dev.off()
--- R/plot.compare_tf_motif_corr.R
+++ R/plot.compare_tf_motif_corr.R
@@ -7,11 +7,44 @@ args <- commandArgs(trailingOnly=TRUE)
 data_file <- args[1]
 plot_file <- args[2]
 
+# read in data
 data <- read.table(data_file)
 
+# jitter the key TFs so that they will be present
+data$Homer.ATAC[rownames(data)=="TP63"]  <- data$Homer.ATAC[rownames(data)=="TP63"] + 0.01
+data$Homer.ATAC[rownames(data)=="ZNF750"]  <- data$Homer.ATAC[rownames(data)=="ZNF750"] + 0.01
+data$Homer.ATAC[rownames(data)=="KLF4"]  <- data$Homer.ATAC[rownames(data)=="KLF4"] + 0.01
+data$Homer.ATAC[rownames(data)=="MAF"]  <- data$Homer.ATAC[rownames(data)=="MAF"] + 0.01
+data$Homer.ATAC[rownames(data)=="MAFB"]  <- data$Homer.ATAC[rownames(data)=="MAFB"] + 0.01
+
+# ignore those for which homer+atac is zero
+data <- data[data$Homer.ATAC != 0,]
+
+# label those that HOMER did better on
+data$labels <- rownames(data)
+data$labels[data$Homer.ATAC <= data$NN] <- ""
+
+# also label key TFs
+data$labels[rownames(data) == "TP63"] <- "TP63"
+data$labels[rownames(data) == "ZNF750"] <- "ZNF750"
+data$labels[rownames(data) == "KLF4"] <- "KLF4"
+data$labels[rownames(data) == "MAFB"] <- "MAFB"
+data$labels[rownames(data) == "MAF"] <- "MAF"
+
+data$labels[rownames(data) == "CREB1"] <- "CREB1"
+data$labels[rownames(data) == "E2F7"] <- "E2F7"
+data$labels[rownames(data) == "NFATC1"] <- "NFATC1"
+data$labels[rownames(data) == "TEAD4"] <- "TEAD4"
+
+# plot
 ggplot(data, aes(x=Homer.ATAC, y=NN)) +
     geom_point(shape=21, fill=NA) +
-    geom_abline(intercept=0, slope=1) +
+    geom_text(aes(x=Homer.ATAC+0.04, label=labels), size=1.5, hjust=0) +
+    geom_abline(intercept=0, slope=1, size=0.115) +
+    labs(
+        title="TF-Motif correlations by Neural Net\nMotifs vs Overlying Accessibility",
+        x="Accessibility to TF expression\ncorrelation",
+        y="NN motifs to TF expression\ncorrelation") +
     theme_bw() +
     theme(
         text=element_text(family="ArialMT"),
